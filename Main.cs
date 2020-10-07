65ee7f02f5c23d76a09d7b8add120ef1c1a2493a
Filip Navara <navara@emclient.com>
2020-10-07 11:05:16 +0200

    Add blank application that uses Microsoft.macOS.Sdk

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using AppKit;
+
+namespace TestApp
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+
+			/*var alert = new NSAlert()
+			{
+				AlertStyle = NSAlertStyle.Critical,
+				InformativeText = "We need to save the document here...",
+				MessageText = "Save Document",
+			};
+			alert.RunModal();*/
+
+			NSApplication.Main(args);
+		}
+	}
+}
